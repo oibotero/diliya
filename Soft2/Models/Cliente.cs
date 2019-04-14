f483e3f0f45d62b80162166392dc07f1f68a0c9a
Lina <linaarias0395@gmail.com>
2019-04-14 17:57:37 -0500

    lina

---
+++ Soft2/Models/Cliente.cs
@@ -0,0 +1,47 @@
+namespace Soft2.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Cliente")]
+    public partial class Cliente
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Cliente()
+        {
+            Tramite = new HashSet<Tramite>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CedulaC { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Nombres { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Apellidos { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Correo { get; set; }
+
+        [Required]
+        [StringLength(13)]
+        public string Contraseña { get; set; }
+
+        [StringLength(20)]
+        public string Telefono { get; set; }
+
+        [StringLength(100)]
+        public string Direccion { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Tramite> Tramite { get; set; }
+    }
+}
