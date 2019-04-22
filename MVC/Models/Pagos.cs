417b3716e5214f4b7adb8a2cf765cdde51707b89
Leonardo Arango <rocker1058@gmail.com>
2019-04-21 19:12:34 -0500

    Leo

---
+++ MVC/Models/Pagos.cs
@@ -0,0 +1,30 @@
+﻿namespace MVC.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Pagos")]
+    public partial class Pagos
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Pagos()
+        {
+            Tramite = new HashSet<Tramite>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Monto { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string MedioPago { get; set; }
+
+       
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Tramite> Tramite { get; set; }
+    }
+}
